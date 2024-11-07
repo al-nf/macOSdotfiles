efccb3d1e1ee7548fe0d67029d5bd64e64403583
afung <afung@alexandria.local>
2024-11-07 12:14:37 -0800

    added wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+----------------
+-- STUFF BEGIN --
+-----------------
+
+config.color_scheme = 'Tokyo Night Moon'
+
+-----------------
+--- STUFF END ---
+-----------------
+return config
